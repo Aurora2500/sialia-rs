517286b3b8ca5a44ee0b2f7cd1b045595900e1ce
Aurora <programming.aurora@gmail.com>
2021-05-24 12:40:43 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! Sialia is a rust library for making twitter bots
+//! that can post tweets, interract with other tweets
+//! and users, upload media, and interract with users
+//! in DMs.
+
+#![deny(missing_docs)]
+
